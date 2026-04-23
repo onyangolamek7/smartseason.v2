ba6b2ec6f02fc0e910b2974ebf3c5a9899c061d3
onyangolamek7 <103209382+onyangolamek7@users.noreply.github.com>
2026-04-23 14:53:01 +0300

    Initial commit

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            'throttle' => ThrottleRequests::class,
             'auth'     => \Illuminate\Auth\Middleware\Authenticate::class,
         ]);
     })
